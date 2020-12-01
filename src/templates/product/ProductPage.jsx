7c1927f903882ba7f4249135cd5cd45fc2c0af80
Pavel <paveliv@gmail.com>
2020-12-01 12:28:34 +0300

    fix: CarouselProvider check if image exists

--- src/templates/product/ProductPage.jsx
+++ src/templates/product/ProductPage.jsx
@@ -107,12 +107,16 @@ function ProductPage({ data, pageContext, location }) {
         naturalSlideWidth={
           productImagesCarouselProps.naturalSlideWidth
             ? productImagesCarouselProps.naturalSlideWidth
-            : images[0].localFile.childImageSharp.main.presentationWidth
+            : images[0]
+            ? images[0].localFile.childImageSharp.main.presentationWidth
+            : 1
         }
         naturalSlideHeight={
           productImagesCarouselProps.naturalSlideHeight
             ? productImagesCarouselProps.naturalSlideHeight
-            : images[0].localFile.childImageSharp.main.presentationHeight
+            : images[0]
+            ? images[0].localFile.childImageSharp.main.presentationHeight
+            : 1
         }
         totalSlides={images.length}
       >
